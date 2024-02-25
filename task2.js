ce7e95ba4d8607344ecf0a8a7aefa632a7e83b74
unknown <Skander@Void.local>
2024-02-25 08:04:06 +0100

    Adding my files

---
+++ task2.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("<h1>Hello Node!!!!</h1>\n");
+
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000!");
+});
